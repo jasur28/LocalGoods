961234e923240ec80939cf9c89a688e84f311112
Jasur89 <jasur_yuldashev@epam.com>
2022-11-23 19:40:04 +0500

    added DBCOntext

---
+++ LocalGoods.DAL/Data/LocalGoodsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LocalGoods.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalGoods.DAL.Data
+{
+    public class LocalGoodsDbContext : DbContext
+    {
+        public virtual DbSet<Farm> Farms { get; set; }
+    }
+}
